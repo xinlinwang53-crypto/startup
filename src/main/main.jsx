6a59194fcfbde0a9c1fb8951ecf9b4c8805959c1
Xinlin Wang <xinlinwang53@gmail.com>
2026-02-12 01:15:15 -0700

    convert login page to react

--- src/main/main.jsx
+++ src/main/main.jsx
@@ -2,8 +2,25 @@ import React from 'react';
 
 export function Main() {
   return (
-    <main className="container-fluid bg-secondary text-center">
-      <div>login displayed here</div>
-    </main>
+    <main className="container-fluid text-center main-theme">
+    <div>
+      <h1><span className="h1-" >Have fun with friends!</span></h1>
+      <form method="get" action="status.html">
+        <div className="input-group mb-3">
+          <span className="input-group-text">@</span>
+          <input className="form-control" type="text" placeholder="your@email.com" />
+        </div>
+        <div className="input-group mb-3">
+          <span className="input-group-text">🔒</span>
+          <input className="form-control" type="password" placeholder="password" />
+        </div>
+        <button type="button" className="btn lg-theme">Login</button>
+        <button type="button" className="btn">Create</button>
+      </form>
+    </div>
+
+
+  </main>
+
   );
 }
