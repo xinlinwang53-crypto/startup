abe046fa6f10bdb8a934d56d15189f5be56560c6
Xinlin Wang <xinlinwang53@gmail.com>
2026-02-12 01:19:12 -0700

    move space to react

--- src/space/space.jsx
+++ src/space/space.jsx
@@ -1,9 +1,70 @@
 import React from 'react';
+import './space.css';
 
 export function Space() {
   return (
-    <main className="container-fluid bg-secondary text-center">
-      <div>login displayed here</div>
-    </main>
+    <main className="container-fluid text-center main-theme">
+    <div className="all-area">
+      <section className="each-avatar">
+        <div className="avatar-">
+          <img className="avatar-" alt="A nice photo" src="/avatar.JPG" />
+
+
+        </div>
+        <div className="statue">
+          <p><span className="font-sta">Ammie</span></p>
+          <span className="font-sta-i">In HBLL</span>
+
+        </div>
+
+
+      </section>
+      <section className="each-avatar">
+        <div className="avatar-">
+          <img className="avatar-" alt="A nice photo" src="/avatar.JPG" />
+
+
+        </div>
+        <div className="statue">
+          <p><span className="font-sta">Ammie</span></p>
+          <span className="font-sta-i">In HBLL</span>
+
+        </div>
+
+
+      </section>
+      <section className="each-avatar">
+        <div className="avatar-">
+          <img className="avatar-" alt="A nice photo" src="/avatar.JPG" />
+
+
+        </div>
+        <div className="statue">
+          <p><span className="font-sta">Ammie</span></p>
+          <span className="font-sta-i">Have lunch</span>
+
+        </div>
+
+
+      </section>
+      <section className="each-avatar">
+        <div className="avatar-">
+
+          <img className="avatar-" alt="A nice photo" src="/avatar.JPG" />
+
+
+        </div>
+        <div className="statue">
+          <p><span className="font-sta">Ammie</span></p>
+          <span className="font-sta-i">In HBLL</span>
+
+        </div>
+
+
+      </section>
+
+    </div>
+
+  </main>
   );
 }
