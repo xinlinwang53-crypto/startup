c918e20c389f363ee73f4a2098874f36eaed2f5a
Xinlin Wang <xinlinwang53@gmail.com>
2026-02-12 00:28:10 -0700

    change app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,46 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './app.css';
-
 export default function App() {
-  return <div className="body bg-dark text-light">App will display here</div>;
+  return (
+    <div className="text-light my-theme">
+      <header className="container-fluid">
+        <nav className="navbar fixed-top navbar-dark">
+          <div className="navbar-brand">
+            StatusTag<sup>&reg;</sup>
+          </div>
+          <menu className="navbar-nav">
+            <li className="nav-item">
+              <a className="nav-link" href="index.html">
+                Home
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="status.html">
+                Status
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="space.html">
+                Space
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="about.html">
+                About
+              </a>
+            </li>
+          </menu>
+        </nav>
+      </header>
+
+      <main>App components go here</main>
+
+      <footer className="bg-dark text-white-50">
+        <div className="container-fluid">
+          <span className="text-reset">Xinlin Wang</span>
+          <a className="text-reset" href="https://github.com/xinlinwang53-crypto/startup.git">
+            Source
+          </a>
+        </div>
+      </footer>
+    </div>
+  );
 }
