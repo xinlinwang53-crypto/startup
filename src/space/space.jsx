d3c4e7c32e509e6c571f5ddebc3f5ba5941cd3ef
Xinlin Wang <xinlinwang53@gmail.com>
2026-02-25 20:12:01 -0700

    add webrocket in space page + new photo

--- src/space/space.jsx
+++ src/space/space.jsx
@@ -1,70 +1,60 @@
+
 import React from 'react';
+
+import { SpaceEvent, SpaceNotifier } from './spaceNotifier';
 import './space.css';
 
-export function Space() {
-  return (
+export function Space(props) {
+  const userName = props.userName;
+
+
+  const A = { id: 'ammie', name: 'Ammie', status: 'In HBLL', avatar: '/avatar.JPG' };
+  const B = { id: 'lily', name: 'Lily', status: 'Studying', avatar: '/avatar.JPG' };
+  const C = { id: 'jimmy', name: 'Jimmy', status: 'Have lunch', avatar: '/dog.jpg' };
+  const [online, setOnline] = React.useState([A,B,C]);
+
+  React.useEffect(() => {
+     function handleSpaceEvent(e) {
+      setOnline((prev) => {
+        if (e.type === SpaceEvent.Join) {
+          const user = e.value.user;
+        
+          if (prev.some((u) => u.id === user.id)) return prev;
+          return [...prev, user];
+        }
+
+        if (e.type === SpaceEvent.Leave) {
+          const user = e.value.user;
+          return prev.filter((u) => u.id !== user.id);
+        }
+
+        return prev;
+      });
+    }
+
+    SpaceNotifier.addHandler(handleSpaceEvent);
+    return () => SpaceNotifier.removeHandler(handleSpaceEvent);
+  }, []);
+
+    return (
     <main className="container-fluid text-center main-theme">
-    <div className="all-area">
-      <section className="each-avatar">
-        <div className="avatar-">
-          <img className="avatar-" alt="A nice photo" src="/avatar.JPG" />
+      <div className="all-area">
+        {online.map((u) => (
+          <section className="each-avatar" key={u.id}>
+            <div className="avatar-">
+              <img className="avatar-" alt={u.name} src={u.avatar} />
+            </div>
 
-
-        </div>
-        <div className="statue">
-          <p><span className="font-sta">Ammie</span></p>
-          <span className="font-sta-i">In HBLL</span>
-
-        </div>
-
-
-      </section>
-      <section className="each-avatar">
-        <div className="avatar-">
-          <img className="avatar-" alt="A nice photo" src="/avatar.JPG" />
-
-
-        </div>
-        <div className="statue">
-          <p><span className="font-sta">Ammie</span></p>
-          <span className="font-sta-i">In HBLL</span>
-
-        </div>
-
-
-      </section>
-      <section className="each-avatar">
-        <div className="avatar-">
-          <img className="avatar-" alt="A nice photo" src="/avatar.JPG" />
-
-
-        </div>
-        <div className="statue">
-          <p><span className="font-sta">Ammie</span></p>
-          <span className="font-sta-i">Have lunch</span>
-
-        </div>
-
-
-      </section>
-      <section className="each-avatar">
-        <div className="avatar-">
-
-          <img className="avatar-" alt="A nice photo" src="/avatar.JPG" />
-
-
-        </div>
-        <div className="statue">
-          <p><span className="font-sta">Ammie</span></p>
-          <span className="font-sta-i">In HBLL</span>
-
-        </div>
-
-
-      </section>
-
-    </div>
-
-  </main>
+            <div className="statue">
+              <p>
+                <span className="font-sta">{u.name}</span>
+              </p>
+              <span className="font-sta-i">{u.status}</span>
+            </div>
+          </section>
+        ))}
+      </div>
+    </main>
   );
 }
+  
---
+++ src/space/spaceNotifier.js
@@ -0,0 +1,59 @@
+const SpaceEvent = {
+  Join: 'join',
+  Leave: 'leave',
+  Status:'status',
+};
+
+class EventMessage {
+  constructor(from, type, value) {
+    this.from = from;
+    this.type = type;
+    this.value = value;
+  }
+}
+
+class SpaceEventNotifier {
+  handlers = [];
+  flip = true;
+
+  constructor() {
+    // Simulate chat messages that will eventually come over WebSocket
+    setInterval(() => {
+      const userC = { id: 'jimmy', name: 'Jimmy', status: 'Have lunch', avatar: '/dog.jpg' };
+      const userD = { id: 'julia', name: 'Julia', status: 'Gym time', avatar: '/dd.jpg' };
+      if (this.flip) {
+        this.broadcastEvent('system', SpaceEvent.Leave, { user: userC });
+        this.broadcastEvent('system', SpaceEvent.Join, { user: userD });
+      } else {
+        this.broadcastEvent('system', SpaceEvent.Leave, { user: userD });
+        this.broadcastEvent('system', SpaceEvent.Join, { user: userC });
+      }
+
+      this.flip = !this.flip;
+    }, 3000);
+  }
+      
+  broadcastEvent(from, type, value) {
+    const event = new EventMessage(from, type, value);
+    this.receiveEvent(event);
+  }
+
+  addHandler(handler) {
+    this.handlers.push(handler);
+  }
+
+  removeHandler(handler) {
+    this.handlers.filter((h) => h !== handler);
+  }
+
+  receiveEvent(event) {
+
+
+    this.handlers.forEach((handler) => {
+      handler(event);
+    });
+  }
+}
+
+const SpaceNotifier = new SpaceEventNotifier();
+export { SpaceEvent, SpaceNotifier };
