aa022dee095b4b72afb38e153115ab5ee078414e
Xinlin Wang <xinlinwang53@gmail.com>
2026-03-12 12:05:42 -0600

    update login backen

--- src/main/authenticated.jsx
+++ src/main/authenticated.jsx
@@ -9,8 +9,16 @@ export function Authenticated(props) {
   const navigate = useNavigate();
 
   function logout() {
-    localStorage.removeItem('userName');
-    props.onLogout();
+    fetch(`/api/auth/logout`, {
+      method: 'delete',
+    })
+      .catch(() => {
+        // Logout failed. Assuming offline
+      })
+      .finally(() => {
+        localStorage.removeItem('userName');
+        props.onLogout();
+      });
   }
 
   return (
