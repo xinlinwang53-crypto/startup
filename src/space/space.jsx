8f0f0661b7148b860209c373688049a90c082eea
Xinlin Wang <xinlinwang53@gmail.com>
2026-03-12 13:38:01 -0600

    make space into fetch

--- src/space/space.jsx
+++ src/space/space.jsx
@@ -6,55 +6,67 @@ import './space.css';
 
 export function Space(props) {
   const username = props.userName.split('@')[0]
-  const currentstatus = localStorage.getItem('mystatus')
+  const currentstatus = localStorage.getItem('mystatus') || 'studying';
 
-  const A = { id:username, name: username, status: currentstatus, avatar: '/avatar.JPG' };
-  const B = { id: 'lily', name: 'Lily', status: 'Studying', avatar: '/avatar.JPG' };
-  const C = { id: 'jimmy', name: 'Jimmy', status: 'Have lunch', avatar: '/dog.jpg' };
-  const [online, setOnline] = React.useState([A,B,C]);
+  const [online, setOnline] = React.useState([]);
 
   React.useEffect(() => {
-     function handleSpaceEvent(e) {
-      setOnline((prev) => {
-        if (e.type === SpaceEvent.Join) {
-          const user = e.value.user;
-        
-          if (prev.some((u) => u.id === user.id)) return prev;
-          return [...prev, user];
-        }
+    fetch('/api/status')
+      .then((res) => res.json())
+      .then((data) => {
+        setOnline(data);
+      })
+      .catch(() => {
 
-        if (e.type === SpaceEvent.Leave) {
-          const user = e.value.user;
-          return prev.filter((u) => u.id !== user.id);
-        }
-
-        return prev;
+        const A = { id: username, name: username, status: currentstatus, avatar: '/avatar.JPG' };
+        const B = { id: 'lily', name: 'Lily', status: 'Studying', avatar: '/avatar.JPG' };
+        const C = { id: 'jimmy', name: 'Jimmy', status: 'Have lunch', avatar: '/dog.jpg' };
+        setOnline([A, B, C]);
       });
-    }
+    },[username,currentstatus]);
 
-    SpaceNotifier.addHandler(handleSpaceEvent);
-    return () => SpaceNotifier.removeHandler(handleSpaceEvent);
-  }, []);
+        /*
+      
+           function handleSpaceEvent(e) {
+            setOnline((prev) => {
+              if (e.type === SpaceEvent.Join) {
+                const user = e.value.user;
+              
+                if (prev.some((u) => u.id === user.id)) return prev;
+                return [...prev, user];
+              }
+      
+              if (e.type === SpaceEvent.Leave) {
+                const user = e.value.user;
+                return prev.filter((u) => u.id !== user.id);
+              }
+      
+              return prev;
+            });
+          }
+      
+          SpaceNotifier.addHandler(handleSpaceEvent);
+          return () => SpaceNotifier.removeHandler(handleSpaceEvent);
+        }, []);
+      */
+        return (
+          <main className="container-fluid text-center main-theme">
+            <div className="all-area">
+              {online.map((u) => (
+                <section className="each-avatar" key={u.id}>
+                  <div className="avatar-">
+                    <img className="avatar-" alt={u.name} src={u.avatar} />
+                  </div>
 
-    return (
-    <main className="container-fluid text-center main-theme">
-      <div className="all-area">
-        {online.map((u) => (
-          <section className="each-avatar" key={u.id}>
-            <div className="avatar-">
-              <img className="avatar-" alt={u.name} src={u.avatar} />
+                  <div className="statue">
+                    <p>
+                      <span className="font-sta">{u.name}</span>
+                    </p>
+                    <span className="font-sta-i">{u.status}</span>
+                  </div>
+                </section>
+              ))}
             </div>
-
-            <div className="statue">
-              <p>
-                <span className="font-sta">{u.name}</span>
-              </p>
-              <span className="font-sta-i">{u.status}</span>
-            </div>
-          </section>
-        ))}
-      </div>
-    </main>
-  );
-}
-  
+          </main>
+        );
+      }
