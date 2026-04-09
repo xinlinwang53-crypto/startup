8f3016941c43c9caf01d7785dfad895ba87fa402
Xinlin Wang <xinlinwang53@gmail.com>
2026-04-09 16:52:24 -0600

    add peerProxy+ add broadcast fucntion

--- service/index.js
+++ service/index.js
@@ -4,6 +4,7 @@ const express = require('express');
 const uuid = require('uuid');
 const app = express();
 const DB = require('./database.js');
+const { peerProxy } = require('./peerProxy.js');
 
 const authCookieName = 'token';
 
